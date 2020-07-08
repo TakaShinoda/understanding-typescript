e386c41c5fed0510f21f5961677a84a20b012a0a
Takahiro <tkhr_621@docomo.ne.jp>
2020-07-08 23:04:35 +0900

    fix

--- app.js
+++ app.js
@@ -1,12 +1,5 @@
-var add = function (n1, n2, showResult) {
-    if (showResult) {
-        console.log(n1 + n2);
-    }
-    else {
-        return n1 + n2;
-    }
+var person = {
+    name: 'takahiro',
+    age: 24
 };
-var number1 = 5;
-var number2 = 2.8;
-var printResult = true;
-var result = add(number1, number2, printResult);
+console.log(person);
---
+++ app.ts
@@ -0,0 +1,7 @@
+const person = {
+    name: 'takahiro',
+    age: 25,
+    hobbies: ['Sports', 'Cooking']
+}
+
+console.log(person)
