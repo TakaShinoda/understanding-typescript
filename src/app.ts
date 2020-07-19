815318453f71c07032f9a035384b0fafc53c854e
Takahiro <tkhr_621@docomo.ne.jp>
2020-07-19 18:36:33 +0900

    関数を追加

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,12 @@ const person = {
     hobbies: ['Sports', 'Cooking']
 }
 
+function add2(n1: number, n2: number){
+    if(n1 + n2 > 0) {
+        return n1 + n2
+    }
+    // return undefined
+}
 
 let age: number
 age = 30
