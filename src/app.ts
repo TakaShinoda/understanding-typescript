fbf690d82d777df504682bc5e9def79f81502c8c
Takahiro <tkhr_621@docomo.ne.jp>
2020-08-17 23:27:44 +0900

    クラスでのインターフェースの実装

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,42 @@
 // プロパティと型を定義、初期値は設定できない
-interface Person {
+// オブジェクトの構造飲み
+// interface Person {
+//   name: string
+//   age: number
+
+//   greet(phrase: string): void
+// }
+
+// type Person = {
+//   name: string
+//   age: number
+
+//   greet(phrase: string): void
+// }
+
+interface Greetable {
   name: string
-  age: number
 
   greet(phrase: string): void
 }
 
-let user1: Person
+// class名の後ろにimplemmentsインターフェースを実装する、複数か
+class Person implements Greetable {
+  name: string
+  age = 30
 
-user1 = {
-  name: 'Max',
-  age: 30,
+  constructor(n: string) {
+    this.name = n
+  }
+
   greet(phrase: string) {
-    console.log(phrase + '' + this.name)
-  },
+    console.log(phrase + ' ' + this.name)
+  }
 }
+
+let user1: Greetable
+
+user1 = new Person('Max')
+
+user1.greet('Hello I am')
+console.log(user1)
